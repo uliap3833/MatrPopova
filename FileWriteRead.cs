7ebaa4f42a075950aead0fadb74304410f26c539
ПоповаЮВ <@TS-06>
2021-09-10 15:32:30 +0300

    Добавьте файлы проекта.

---
+++ FileWriteRead.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Matrix
+{
+    class FileWriteRead
+    {
+        public static void ReadData(string path, ref int[,] matr)
+        {
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Файл не найден!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            var lines = File.ReadAllLines(path);
+            int k = 0;
+            string[] strLen = lines[0].Split(';'); //Определение длины строки массива
+            matr = new int[lines.Length, strLen.Length]; //Инициализируется массив, куда будут записываться значения
+            foreach (var line in lines)
+            {
+
+                string[] str = line.Split(';'); //Считывание в строковой массив
+                for (int j = 0; j < str.Length; j++)
+                {
+                    matr[k, j] = Convert.ToInt32(str[j]); //Передача в основной массив
+                }
+                k++;
+            }
+        }
+
+        public static void WriteToFile(string path, List<int> neededNums, int count)
+        {
+            if (!File.Exists(path)) File.Create(path).Close();
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+
+                sw.WriteLine("Элементы массива, у которых сумма кубов их цифр равна самому числу:");
+                foreach(var num in neededNums)
+                {
+                    sw.Write(num+"; ");
+                }
+                sw.WriteLine("\nОбщее количество элементов, подходящих по условию: " +count);
+            }
+        }
+    }
+}
---
+++ Proga.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Proga
+    {
+        string readPath; //Путь читаемого файла
+        string savingPath; //Путь файла, в который идёт запись
+        int[,] matrix; //Матрица
+        int count = 0; //Количество подходящих чисел
+        List<int> neededNums = new List<int>(); //Список подходящих чисел
+
+        public Proga(string readPath, string savingPath) //Конструктор класса
+        {
+            this.readPath = readPath;
+            this.savingPath = savingPath;
+        }
+
+        void MatrixMoving() //Прохождение по всей матрице
+        {  
+            foreach(int num in matrix)
+            {
+                if(NumSumCheck(num) == true)
+                {
+                    count++;
+                    neededNums.Add(num);
+                }
+            }
+        }
+
+        bool NumSumCheck(int num) //Проверка элемента по условию
+        {
+            int sum = 0, origNum = num;
+            bool result = false;
+            while (num > 0)
+            {
+                sum = sum + Convert.ToInt32(Math.Pow(num % 10,3));
+                num = num / 10;
+            }
+
+            if (sum == origNum) result = true;
+
+            return result;
+        }
+
+        public void CalcStart() //Метод, вызывающий функционал класса, а также чтение и запись
+        {
+            FileWriteRead.ReadData(readPath, ref matrix);
+            MatrixMoving();
+            FileWriteRead.WriteToFile(savingPath, neededNums, count);
+        }
+    }
+
+}
